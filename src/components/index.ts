84821dddb310189885ab6775bf181b3ce2706c4e
Nabarun <nicknabarun@gmail.com>
2024-01-04 00:50:47 +0530

    Export new navigation components from index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,5 @@
 export { default as MaxWidthWrapper } from "./MaxWidthWrapper";
+export { default as Navbar } from "./Navbar";
+export { default as Icons } from "./Icons";
+export { default as NavItems } from "./NavItems";
+export { default as NavItem } from "./NavItem";
