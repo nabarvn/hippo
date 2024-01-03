b37571269721134175442793fcbaccd1d6a677c6
Nabarun <nicknabarun@gmail.com>
2024-01-04 00:49:01 +0530

    Integrate `Navbar` into `RootLayout`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
 import { cn, constructMetadata } from "@/lib/utils";
+import { Navbar } from "@/components";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
         className={cn("relative h-full font-sans antialiased", inter.className)}
       >
         <main className='relative flex flex-col min-h-screen'>
+          <Navbar />
           <div className='flex-grow flex-1'>{children}</div>
         </main>
       </body>
