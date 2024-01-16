5289de30991c03b5ff445d8dce93db96c3a57e10
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-01-16 22:56:37 +0530

    Integrate tRPC with Express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import { appRouter } from "@/trpc";
 import { getPayloadClient } from "./payload-client";
 import { nextApp, nextHandler } from "./next-utils";
+import * as trpcExpress from "@trpc/server/adapters/express";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
+
+// making the requests/responses available in Next.js wherever we handle the api calls from tRPC
+const createContext = ({
+  req,
+  res,
+}: trpcExpress.CreateExpressContextOptions) => ({
+  req,
+  res,
+});
 
 const start = async () => {
   const payload = await getPayloadClient({
@@ -14,6 +25,17 @@ const start = async () => {
       },
     },
   });
+
+  // forward the client requests to tRPC in Next.js
+  app.use(
+    "/api/trpc",
+    trpcExpress.createExpressMiddleware({
+      router: appRouter,
+
+      // allows us to take the requests/responses that we get in Express and attach them to context for using in Next.js
+      createContext,
+    })
+  );
 
   // adding a middleware to the Express app
   // this setup is typical for integrating Express with Next.js
