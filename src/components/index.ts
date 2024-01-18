ca1b928d62311dbf5912325ec86f6b68a4619dad
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-01-18 23:38:11 +0530

    Add `VerifyEmail` component export

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as NavItems } from "./NavItems";
 export { default as NavItem } from "./NavItem";
 export { default as Cart } from "./Cart";
 export { default as Providers } from "./Providers";
+export { default as VerifyEmail } from "./VerifyEmail";
