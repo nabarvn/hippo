ae746ec44da6420121f42c270d1cd6e7b6ed5383
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-01-30 23:52:05 +0530

    Expand payload collections with new entities

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,10 +1,17 @@
 import path from "path";
 import dotenv from "dotenv";
-import { Users } from "../src/collections";
 import { buildConfig } from "payload/config";
 import { slateEditor } from "@payloadcms/richtext-slate";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
+
+import {
+  Users,
+  Products,
+  Media,
+  ProductFile,
+  Orders,
+} from "../src/collections";
 
 // loading environment variables
 dotenv.config({
@@ -13,7 +20,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products, Media, ProductFile, Orders],
   routes: {
     admin: "/sell",
   },
