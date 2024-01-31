9a0f7c04a2c8b08b680d37544b56ed06c7b2a431
Nabarun <64539836+nabarvn@users.noreply.github.com>
2024-02-01 00:18:11 +0530

    Add responsive image slider component

---
+++ src/components/ImageSlider.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+import type SwiperType from "swiper";
+import { Pagination } from "swiper/modules";
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+interface ImageSliderProps {
+  urls: string[];
+}
+
+const ImageSlider = ({ urls }: ImageSliderProps) => {
+  const [swiper, setSwiper] = useState<null | SwiperType>(null);
+
+  // this state is actually provided by swiper
+  // it's not reactive - hence we need to keep track of it
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  // tracking the array image position
+  const [slideConfig, setSlideConfig] = useState({
+    isBeginning: true,
+    isEnd: activeIndex === (urls.length ?? 0) - 1,
+  });
+
+  useEffect(() => {
+    swiper?.on("slideChange", ({ activeIndex }) => {
+      setActiveIndex(activeIndex);
+
+      // keeping track of each slide
+      setSlideConfig({
+        isBeginning: activeIndex === 0,
+        isEnd: activeIndex === (urls.length ?? 0) - 1,
+      });
+    });
+  }, [swiper, urls]);
+
+  const activeStyles =
+    "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 z-50 place-items-center rounded-full border-2 bg-white border-zinc-300";
+
+  const inactiveStyles = "hidden text-gray-400";
+
+  return (
+    <div className='group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl'>
+      <div className='absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition'>
+        <button
+          aria-label='next image'
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slideNext();
+          }}
+          className={cn(activeStyles, "right-3 transition", {
+            [inactiveStyles]: slideConfig.isEnd,
+
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slideConfig.isEnd,
+          })}
+        >
+          <ChevronRight className='h-4 w-4 text-zinc-700' />
+        </button>
+
+        <button
+          aria-label='previous image'
+          onClick={(e) => {
+            e.preventDefault();
+            swiper?.slidePrev();
+          }}
+          className={cn(activeStyles, "left-3 transition", {
+            [inactiveStyles]: slideConfig.isBeginning,
+
+            "hover:bg-primary-300 text-primary-800 opacity-100":
+              !slideConfig.isBeginning,
+          })}
+        >
+          <ChevronLeft className='h-4 w-4 text-zinc-700' />
+        </button>
+      </div>
+
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={50}
+        modules={[Pagination]}
+        onSwiper={(swiper) => setSwiper(swiper)}
+        className='h-full w-full'
+        pagination={{
+          renderBullet: (_, className) => {
+            return `<span class="rounded-full transition ${className}"></span>`;
+          },
+        }}
+      >
+        {urls.map((url, i) => (
+          <SwiperSlide key={i} className='-z-10 relative h-full w-full'>
+            <Image
+              fill
+              src={url}
+              loading='eager'
+              alt='Product image'
+              className='-z-10 h-full w-full object-cover object-center'
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
+
+export default ImageSlider;
